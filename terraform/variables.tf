a894c7dda39215b654192ca03ff1ebd833915532
amar.s <sameeranamarnath@gmail.com>
2023-11-22 18:42:21 +0530

    vpcs and subnet vals

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,18 +1,18 @@
 variable "aws_vpc_id" {
   type        = string
   description = "AWS VPC ID deployed from network repo"
-  default     = "vpc-0ea61ea098cf5f747"
+  default     = "vpc-004cb78e558bde3ed"
 }
 
 variable "aws_public_subnet_ids" {
   description = "public subnet ids"
-  default     = ["subnet-0c7a3fd95e72dc68b", "subnet-06074be1d5b4f7c22"] # CHANGE TO YOUR SUBNET IDS
+  default     = ["subnet-0e2f0f7a73f5c3dea", "subnet-012eaa330a2fc9cdd"] # CHANGE TO YOUR SUBNET IDS
 }
 
 
 variable "aws_private_subnet_ids" {
   description = "private subnet ids"
-  default     = ["subnet-0e83578144e584857", "subnet-0f9028bcdc2796e4c"] # CHANGE TO YOUR SUBNET IDS
+  default     = ["subnet-08325656ff40174fd", "subnet-0811ce5fe6efe9151"] # CHANGE TO YOUR SUBNET IDS
 }
 
 variable "aws_region" {
